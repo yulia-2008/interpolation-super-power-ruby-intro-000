50e9730ecb81e2302da5ec279acc0bcb1b933ffe
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-09 14:17:16 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,4 +2,4 @@
  def display_rainbow(color)
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
-display_rainbow("colors")
+display_rainbow(colors)
